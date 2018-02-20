b2eec018479b558f36395942ec67de7ba6743463
Sergio S. Ribeiro <ssr.informatica@gmail.com>
2018-02-20 12:03:32 -0600

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,12 @@
+import sys
+import os.path 
+import bib2csv
 
+print 'Converting from: '+sys.argv[1]+' to: '+sys.argv[2]  
+
+if os.path.exists(sys.argv[1]):
+    objBib2csv = Bib2csv()    
+    objBib2csv.setBibfile( open(sys.argv[1],'r').read() )
+    objBib2csv.generate(sys.argv[2])
+else:
+    print "Error reading file "+sys.argv[1]
